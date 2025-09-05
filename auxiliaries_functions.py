b640746a9094e60b0a8a62f1173066441ea4bbcf
Ricken Santos <dinizrickenn@gmail.com>
2025-09-05 20:13:14 -0300

    feat: auxiliaries_functions

---
+++ auxiliaries_functions.py
@@ -0,0 +1,25 @@
+def reset_square_matrix(n):
+    M = [[0 for _ in range(n)] for _ in range(n)]
+    return M
+
+def deepcopy(M):
+    matrix = []
+
+    for i in range(len(M)):
+        matrix.append([])
+        for e in [i]:
+            matrix[i].append(e)
+
+    return matrix
+
+def show_matrix(M):
+
+    for i in range(len(M)):
+        for j in range(len(M[i])):
+            
+            if M[i][j] == 1:
+                M[i][j] = f'\033[94m{1}\033[0m'
+            elif[i][j] == 2:
+                M[i][j] = f'\033[91m{2}\033[0m'
+            else:
+                M[i][j] = f'{M[i][j]}'
