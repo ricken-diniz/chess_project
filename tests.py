0bb72c420e68fe16f6ab29f274a1c6fc4e5753ed
Ricken Santos <dinizrickenn@gmail.com>
2025-09-07 12:21:58 -0300

    refactor: encapsulando função de testes para organização

--- tests.py
+++ tests.py
@@ -1,80 +1,54 @@
 from pieces_movement import *
 from aux_functions import *
 
-def test_simple_moves(d, points, ispawn = False):
-    for tuple in points:
-        i,j,n = tuple
+class FeaturesTests():
 
-        M = get_square_matrix(8)  
+    def test_simple_moves(d, points, ispawn = False):
+        for tuple in points:
+            i,j,n = tuple
 
-        d(i,j,M) if not ispawn else d(i,j,M,n)
-        show_matrix(M)
-        print(tuple)
-        for l in M:
-            print(' '.join(l))
-        print()
+            M = get_square_matrix(8)  
 
-def test_compound_moves(d, points):
-    for tuple in points:
-        i,j,n = tuple
+            d(i,j,M) if not ispawn else d(i,j,M,n)
+            show_matrix(M)
+            print(tuple)
+            for l in M:
+                print(' '.join(l))
+            print()
 
-        chessboard = get_game_example() 
-        M = get_square_matrix(8)   
+    def test_compound_moves(d, points):
+        for tuple in points:
+            i,j,n = tuple
 
-        d(i,j,M,chessboard,n)
-        show_matrix(M)
-        print(tuple)
-        for l in M:
-            print(' '.join(l))
-        print()
+            chessboard = get_game_example() 
+            M = get_square_matrix(8)   
 
-def test_initial_game(defs):
-    chessboard = get_game_example()
+            d(i,j,M,chessboard,n)
+            show_matrix(M)
+            print(tuple)
+            for l in M:
+                print(' '.join(l))
+            print()
 
-    for lin in range(len(chessboard)):
-        for col in range(len(chessboard[lin])):
+    def test_initial_game(defs):
+        chessboard = get_game_example()
 
-            try:
-                color = check_color(chessboard[lin][col])
-                for k in chessboard[lin][col].keys():
-                    piece = k
-                    break
-                chessboard[lin][col][piece] = get_square_matrix(8)
+        for lin in range(len(chessboard)):
+            for col in range(len(chessboard[lin])):
 
-                defs[piece.lower()](lin,col, chessboard[lin][col][piece],chessboard, color)
+                try:
+                    color = check_color(chessboard[lin][col])
+                    for k in chessboard[lin][col].keys():
+                        piece = k
+                        break
+                    chessboard[lin][col][piece] = get_square_matrix(8)
 
-            except Exception as x:
-                continue
-    
-    maps = []
-    for line in chessboard:
-        lin = []
-        for e in line:
-            if type(e) == dict:
-                for k in e.keys():  
-                    lin.append(k)
-                    break
-                maps.append(e)
-            else:
-                lin.append(e)
+                    defs[piece.lower()](lin,col, chessboard[lin][col][piece],chessboard, color)
 
-        print(' '.join(lin))
+                except Exception as x:
+                    continue
 
-
-    for e in maps:
-        for k in e.keys():
-            piece = k
-            break
-
-        print(f'\n{'':=^30}\n')
-        print(f'{piece:=^30}\n')
-
-        show_matrix(e[piece])
-
-        for lin in e[piece]:
-            for _ in range(len(lin)):
-                lin[_] = str(lin[_])
-            print(' '.join(lin))
+        return chessboard
 
 def main():
     
@@ -107,25 +81,60 @@ def main():
         'p': spawn_pointers_pawn,
     }
     
-    # for e in defs.keys():
-    #     piece = f'  {e}  '
-    #     print(f'\n{'':=^30}\n')
-    #     print(f'{piece:=^30}\n')
-    #     if e == 'pawn':
-    #         test_simple_moves(defs[e], points, True)
-    #     else:
-    #         test_simple_moves(defs[e], points)
+    # with FeaturesTests as ft:
 
-    # for e in defs.keys():
-    #     piece = f'  {e}  '
-    #     print(f'\n{'':=^30}\n')
-    #     print(f'{piece:=^30}\n')
-    #     # if e == 'pawn':
-    #     #     test_compound_moves(defs[e], points, True)
-        
-    #     test_compound_moves(defs[e], points)
+    # # ==================Simple Moves==================
+    #     for e in defs.keys():
+    #         piece = f'  {e}  '
+    #         print(f'\n{'':=^30}\n')
+    #         print(f'{piece:=^30}\n')
+    #         if e == 'pawn':
+    #             ft.test_simple_moves(defs[e], points, True)
+    #         else:
+    #             ft.test_simple_moves(defs[e], points)
 
-    test_initial_game(defs)
+    # # ==================Compound Moves==================
+    #     for e in defs.keys():
+    #         piece = f'  {e}  '
+    #         print(f'\n{'':=^30}\n')
+    #         print(f'{piece:=^30}\n')
+    #         # if e == 'pawn':
+    #         #     ft.test_compound_moves(defs[e], points, True)
+            
+    #         ft.test_compound_moves(defs[e], points)
+
+
+    # ==================Initial Game==================
+    chessboard = get_initial_game(defs)
+
+    maps = []
+    for line in chessboard:
+        lin = []
+        for e in line:
+            if type(e) == dict:
+                for k in e.keys():  
+                    lin.append(k)
+                    break
+                maps.append(e)
+            else:
+                lin.append(e)
+
+        print(' '.join(lin))
+
+    for e in maps:
+        for k in e.keys():
+            piece = k
+            break
+
+        print(f'\n{'':=^30}\n')
+        print(f'{piece:=^30}\n')
+
+        show_matrix(e[piece])
+
+        for lin in e[piece]:
+            for _ in range(len(lin)):
+                lin[_] = str(lin[_])
+            print(' '.join(lin))
 
 
 
